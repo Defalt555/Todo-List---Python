97f2aed75297d8658c6939713638516601dd26fb
Defalt <spec>
2026-03-05 21:57:14 +0100

    update on Tkinter

--- src/tkinter/main_window.py
+++ src/tkinter/main_window.py
@@ -1,10 +1,10 @@
 from tkinter import *
 from tkinter import ttk
-
 root = Tk()
-root.title("Todo List")
+root.title("Todo List Manager")
 
 mainframe = ttk.Frame(root, padding=(6,6,12,12))
 
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 
+button = ttk.Button(mainframe)
